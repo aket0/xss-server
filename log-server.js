ced3aa19df22ede5f3b6370665a1bfbbab8c2909
aket0 <moynehubert@gmail.com>
2025-05-27 11:39:35 +0200

    cors

--- log-server.js
+++ log-server.js
@@ -1,5 +1,8 @@
 const express = require('express');
 const app = express();
+const cors = require('cors');
+
+app.use(cors());
 
 app.get('/log', (req, res) => {
   // Extraire les paramètres de la requête
